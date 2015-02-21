8ae537e96baed7f2e8e5d6fe5b503b6d77e65834
garricktlee <garricktlee89@gmail.com>
2015-02-20 23:55:23 -0800

    new pages

---
+++ addition.js
@@ -0,0 +1,6 @@
+
+if (confirm("Addition Calculator") === true) {
+	var sum = parseInt(prompt("Enter an Int"));
+	sum += parseInt(prompt("Enter 2nd Int"));
+	document.write(sum);
+}
--- main.js
+++
@@ -1,8 +0,0 @@
-var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Test Calculator';
-
-if (confirm("Addition Calculator") === true) {
-	var sum = parseInt(prompt("Enter an Int"));
-	sum += parseInt(prompt("Enter 2nd Int"));
-	document.write(sum);
-}
