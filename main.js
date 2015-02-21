bea7b8b27e695b5a0ac14798c4afbd06422bbb42
Henry Ammermann <hintsss@gmail.com>
2015-02-20 23:43:27 -0800

    minor changes

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 var myHeading = document.querySelector('h1');
 myHeading.innerHTML = 'Test Calculator';
 
-var sum = 0;
-
-if(confirm("Addition Calculator") === true){
-	sum = parseInt(prompt("Enter an Int"));
+if (confirm("Addition Calculator") === true) {
+	var sum = parseInt(prompt("Enter an Int"));
 	sum += parseInt(prompt("Enter 2nd Int"));
 	document.write(sum);
 }
